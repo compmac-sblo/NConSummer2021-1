49bbe2a24917efe071e435a13c2b7718b4f153c8
compmac-sblo <mail@td5.so-net.ne.jp>
2021-08-31 17:05:57 +0900

    IDB+WORKERのテストを改良

--- WorkerTest.js
+++ WorkerTest.js
@@ -1,27 +1,39 @@
+
+// main.jsからのpostMessageを受取る
 self.addEventListener('message', function(e) {
   let cmd = e.data.cmd;
-//  self.postMessage(e.data);
+  //  self.postMessage(e.data);
+
+  // DBの名前
+  const dbName = "the_name";
+  // 受取ったDataを変数に格納
+  const customerData = e.data.msg;
+  // DBに接続、または無い場合には新規作成
+  let request = indexedDB.open(dbName, 2);
+
+  // DBの作成に失敗した場合
+  request.onerror = function(event) {
+    // エラー処理
+    self.postMessage({'cmd': 'error', 'msg': 'データベースの作成・接続に失敗'});
+    };
+
   switch (cmd) {
     case "TEST":
       self.postMessage({'cmd': 'TEST', 'msg': e.data.msg});
       break;
     case "DB":
-      const dbName = "the_name";
-      const customerData = e.data.msg;
-      let request = indexedDB.open(dbName, 2);
-      request.onerror = function(event) {
-        // エラー処理
-        self.postMessage({'cmd': 'error', 'msg': 'データベースの作成失敗'});
-      };
+      
+      // DB作成成功後の処理
       request.onupgradeneeded = function(event) {
+      
         self.postMessage({'cmd': 'success', 'msg': 'DB作成成功'});
         self.postMessage({'cmd': 'success', 'msg': customerData});
-        let db = event.target.result;
+        const db = event.target.result;
         
         // 顧客の情報を保存する objectStore を作成します。
         // "ssn" は一意であることが保証されていますので、キーパスとして使用します。
         // あるいは少なくとも、キックオフミーティングで言われたことです。
-        let objectStore = db.createObjectStore("customers", { keyPath: "ssn" });
+        const objectStore = db.createObjectStore("customers", { keyPath: "ssn" });
       
         // 顧客を名前で検索するためのインデックスを作成します。
         // 重複する可能性がありますので、一意のインデックスとしては使用できません。
@@ -33,19 +45,34 @@ self.addEventListener('message', function(e) {
       
         // データを追加する前に objectStore の作成を完了させるため、
         // transaction oncomplete を使用します。
-        objectStore.transaction.oncomplete = function(event) {
+//        objectStore.transaction.oncomplete = function(event) {
           // 新たに作成した objectStore に値を保存します。
-          let customerObjectStore = db.transaction("customers", "readwrite").objectStore("customers");
+//          const customerObjectStore = db.transaction("customers", "readwrite").objectStore("customers");
+//          customerData.forEach(function(customer) {
+//            self.postMessage({'cmd': 'customer', 'msg': customer});
+//            customerObjectStore.add(customer);
+//          });
+//        };
+        //db.close();
+      };
+      self.postMessage({'cmd': 'success', 'msg': 'データベースに保存成功'});
+      break;
+    case "DB2":
+      // 接続に成功したら
+      request.onsuccess = function(event){
+        const db = event.target.result;
+
+        const customerObjectStore = db.transaction("customers", "readwrite").objectStore("customers");
           customerData.forEach(function(customer) {
             self.postMessage({'cmd': 'customer', 'msg': customer});
             customerObjectStore.add(customer);
           });
-        };
+          db.close();
       };
-      this.self.postMessage({'cmd': 'success', 'msg': 'データベースに保存成功'});
+      this.self.postMessage({'cmd': 'success', 'msg': 'データの保存成功'});
       break;
-
     default:
       break;
-  }
+  };
+  
 }, false);
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Moduleの読込
 import * as test from './ModuleTest1.js'
 
 
@@ -7,13 +8,31 @@ test.test2();
 test.test3();
 test.test4();
 
-console.log("worker");
+// indexedDBが使用出来るかどうかの確認
+if (!window.indexedDB) {
+  window.console.log("このブラウザーは安定版の IndexedDB を対応していません。IndexedDB の機能は利用できません。");
+}
 
-
-
-
+// DataBase操作を別スレッドに移す為にWorkerを作成
 const worker = new Worker('WorkerTest.js');
 
+// 顧客データがどのようなものかを示します
+const customerData = [
+  { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
+  { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" }
+];
+const customerData2 = [
+  { ssn: "666-44-4444", name: "Any", age: 35, email: "any@company.com" },
+  { ssn: "777-55-5555", name: "June", age: 32, email: "june@home.org" }
+];
+
+
+// データをDataBaseに保存
+worker.postMessage({'cmd': 'DB', 'msg': customerData});
+worker.postMessage({'cmd': 'DB2', 'msg': customerData});
+worker.postMessage({'cmd': 'DB2', 'msg': customerData2});
+
+// Workerから戻って来たものを分類と実行
 worker.addEventListener('message', function(e) {
   console.log('Worker said: ', e.data);
   const cmd = e.data.cmd;
@@ -38,19 +57,4 @@ worker.addEventListener('message', function(e) {
   }
 }, false);
 
-worker.postMessage('Hello World');
-worker.postMessage({ 'cmd': 'TEST', 'msg': 'TESTmsg' });
-
-
-if (!window.indexedDB) {
-  window.console.log("このブラウザーは安定版の IndexedDB を対応していません。IndexedDB の機能は利用できません。");
-}
-
-
-// 顧客データがどのようなものかを示します
-const customerData = [
-  { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
-  { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" }
-];
-
-worker.postMessage({'cmd': 'DB', 'msg': customerData});
+//worker.postMessage({ 'cmd': 'TEST', 'msg': 'TESTmsg' });
