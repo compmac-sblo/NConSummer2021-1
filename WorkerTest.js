8d9d69dee94f6b3723f06e8b3ed1ccb3962c6b61
compmac-sblo <mail@td5.so-net.ne.jp>
2021-08-31 16:04:50 +0900

    IDBをWorkerで動かすテスト

--- WorkerTest.js
+++ WorkerTest.js
@@ -1,6 +1,51 @@
 self.addEventListener('message', function(e) {
-  self.postMessage(e.data);
-  if (e.data.cmd === "TEST"){
-    self.postMessage(e.data.msg);
+  let cmd = e.data.cmd;
+//  self.postMessage(e.data);
+  switch (cmd) {
+    case "TEST":
+      self.postMessage({'cmd': 'TEST', 'msg': e.data.msg});
+      break;
+    case "DB":
+      const dbName = "the_name";
+      const customerData = e.data.msg;
+      let request = indexedDB.open(dbName, 2);
+      request.onerror = function(event) {
+        // エラー処理
+        self.postMessage({'cmd': 'error', 'msg': 'データベースの作成失敗'});
+      };
+      request.onupgradeneeded = function(event) {
+        self.postMessage({'cmd': 'success', 'msg': 'DB作成成功'});
+        self.postMessage({'cmd': 'success', 'msg': customerData});
+        let db = event.target.result;
+        
+        // 顧客の情報を保存する objectStore を作成します。
+        // "ssn" は一意であることが保証されていますので、キーパスとして使用します。
+        // あるいは少なくとも、キックオフミーティングで言われたことです。
+        let objectStore = db.createObjectStore("customers", { keyPath: "ssn" });
+      
+        // 顧客を名前で検索するためのインデックスを作成します。
+        // 重複する可能性がありますので、一意のインデックスとしては使用できません。
+        objectStore.createIndex("name", "name", { unique: false });
+      
+        // 顧客をメールアドレスで検索するためのインデックスを作成します。2 人の顧客が同じメールアドレスを
+        // 使用しないようにしたいので、一意のインデックスを使用します。
+        objectStore.createIndex("email", "email", { unique: true });
+      
+        // データを追加する前に objectStore の作成を完了させるため、
+        // transaction oncomplete を使用します。
+        objectStore.transaction.oncomplete = function(event) {
+          // 新たに作成した objectStore に値を保存します。
+          let customerObjectStore = db.transaction("customers", "readwrite").objectStore("customers");
+          customerData.forEach(function(customer) {
+            self.postMessage({'cmd': 'customer', 'msg': customer});
+            customerObjectStore.add(customer);
+          });
+        };
+      };
+      this.self.postMessage({'cmd': 'success', 'msg': 'データベースに保存成功'});
+      break;
+
+    default:
+      break;
   }
 }, false);
--- main.js
+++ main.js
@@ -8,14 +8,38 @@ test.test3();
 test.test4();
 
 console.log("worker");
+
+
+
+
 const worker = new Worker('WorkerTest.js');
 
 worker.addEventListener('message', function(e) {
   console.log('Worker said: ', e.data);
+  const cmd = e.data.cmd;
+  switch (cmd) {
+    case 'TEST':
+      console.log(e.data.msg);
+      break;
+    case 'error':
+      console.log(e.data.msg);
+      break;
+    case 'success':
+      console.log(e.data.msg);
+      break;
+    case 'db':
+      console.log(e.data.msg);
+      break;
+    case 'customer':
+      console.log(e.data.msg);
+      break;
+    default:
+      break;
+  }
 }, false);
 
 worker.postMessage('Hello World');
-worker.postMessage({ 'cmd': 'TEST', 'msg': 'msg' });
+worker.postMessage({ 'cmd': 'TEST', 'msg': 'TESTmsg' });
 
 
 if (!window.indexedDB) {
@@ -29,39 +53,4 @@ const customerData = [
   { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" }
 ];
 
-const dbName = "the_name";
-
-let request = indexedDB.open(dbName, 2);
-
-request.onerror = function(event) {
-  // エラー処理
-  console.log("データベースの作成失敗");
-};
-request.onupgradeneeded = function(event) {
-  console.log("DB作成成功");
-  let db = event.target.result;
-  console.log(db);
-  // 顧客の情報を保存する objectStore を作成します。
-  // "ssn" は一意であることが保証されていますので、キーパスとして使用します。
-  // あるいは少なくとも、キックオフミーティングで言われたことです。
-  let objectStore = db.createObjectStore("customers", { keyPath: "ssn" });
-
-  // 顧客を名前で検索するためのインデックスを作成します。
-  // 重複する可能性がありますので、一意のインデックスとしては使用できません。
-  objectStore.createIndex("name", "name", { unique: false });
-
-  // 顧客をメールアドレスで検索するためのインデックスを作成します。2 人の顧客が同じメールアドレスを
-  // 使用しないようにしたいので、一意のインデックスを使用します。
-  objectStore.createIndex("email", "email", { unique: true });
-
-  // データを追加する前に objectStore の作成を完了させるため、
-  // transaction oncomplete を使用します。
-  objectStore.transaction.oncomplete = function(event) {
-    // 新たに作成した objectStore に値を保存します。
-    let customerObjectStore = db.transaction("customers", "readwrite").objectStore("customers");
-    customerData.forEach(function(customer) {
-      console.log(customer);
-      customerObjectStore.add(customer);
-    });
-  };
-};
+worker.postMessage({'cmd': 'DB', 'msg': customerData});
